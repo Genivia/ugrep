465a2e4d68f420a774d47256ac25aedd7034bf93
Robert van Engelen <ravengelen@gmail.com>
2019-04-28 21:42:02 -0400

    Universal grep - a Unicode-aware grep utility

--- ugrep.cpp
+++ ugrep.cpp
@@ -24,6 +24,7 @@
 * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR      *
 * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF       *
 * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                                   *
+\******************************************************************************/
 
 /**
 @file      ugrep.cpp
