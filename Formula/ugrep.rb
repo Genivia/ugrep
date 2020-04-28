3f927e43258d8ac89933ff031259a0c984fca53f
Robert van Engelen <ravengelen@gmail.com>
2020-04-28 15:29:04 -0400

    formula for Homebrew

---
+++ Formula/ugrep.rb
@@ -0,0 +1,21 @@
+class Ugrep < Formula
+  desc "A search tool like grep but fast and interactive"
+  homepage "https://github.com/Genivia/ugrep"
+  url "https://github.com/Genivia/ugrep/archive/v2.0.6.tar.gz"
+  sha256 "c51f87b8c57d0705b730437927b2e3fd459771885c50115c5ce39b6943cdf653"
+
+  depends_on "pcre2"
+  depends_on "xz"
+
+  def install
+    system "./configure", "--enable-color",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system "make", "test"
+  end
+end
