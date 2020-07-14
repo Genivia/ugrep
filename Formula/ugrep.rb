eb8c2ebd7653cc4a97a40a33bb54c72883b01f01
Robert van Engelen <ravengelen@gmail.com>
2020-07-14 16:56:57 -0400

    updated Homebrew formula to v2.4.1

--- Formula/ugrep.rb
+++ Formula/ugrep.rb
@@ -2,8 +2,8 @@ class Ugrep < Formula
   env :std
   desc "Ultra fast grep with query UI, fuzzy search, archive search, and more"
   homepage "https://github.com/Genivia/ugrep"
-  url "https://github.com/Genivia/ugrep/archive/v2.4.0.tar.gz"
-  sha256 "be7dc40c6130d3bbb669a79f9e5c6812be421d298cb591d6f9a5f6d1469dc401"
+  url "https://github.com/Genivia/ugrep/archive/v2.4.1.tar.gz"
+  sha256 "88fd84ae93a0be914350f1c2ab73086fd5f4a87963d00685a596327f03cb1e83"
 
   depends_on "pcre2"
   depends_on "xz"
