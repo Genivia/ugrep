8a462ca487e85a5523f4b91273d8d233637805a2
Hartmut Goebel <h.goebel@crazy-compilers.com>
2021-01-06 00:24:18 +0100

    tests: Generate encoded "lorem" files from a single source.
    Using a single source for files with the same content just using
    different encoding is always a good thing.  In this case it showed
    that the UTF-16 and UTF-18 files could not be easily reproduced from
    the latin1 encoded file:  These files include a BOM and are
    big-endian.  When trying to recreate these files on my machine, I got
    litte-endian files at first.

--- tests/gen-test-inputs.sh
+++ tests/gen-test-inputs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# requires: tar, compress, pax, cpio, gzip, bzip2, lzma, xz, lz4, zip
+# requires: tar, compress, pax, cpio, gzip, bzip2, lzma, xz, lz4, zip,
+# iconv
 
 echo "GENERATING SIMPLE FILES"
 
@@ -12,6 +13,17 @@ dolor
 sit
 amét
 END
+
+
+echo "GENERATING LOREM FILES"
+
+iconv --from-code latin1 --to-code UTF-8    >  lorem.utf8.txt  lorem.latin1.txt
+# UTF-16 in big-endian including a BOM indicating big-endian
+echo -ne '\xFE\xFF' > lorem.utf16.txt
+iconv --from-code latin1 --to-code UTF-16BE >> lorem.utf16.txt lorem.latin1.txt
+# UTF-32 in big-endian including a BOM indicating big-endian
+echo -ne '\x00\x00\xFE\xFF' > lorem.utf32.txt
+iconv --from-code latin1 --to-code UTF-32BE >> lorem.utf32.txt lorem.latin1.txt
 
 
 
