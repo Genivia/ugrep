30d6be9f410e24c2f23195dab7beed993577fac4
Robert van Engelen <genivia-inc@users.noreply.github.com>
2023-08-21 20:06:21 -0400

    update makemake

--- makemake.sh
+++ makemake.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
 # if RE/flex was installed below this directory, then copy its files
-if [ -d ../reflex ]; then
-echo "Copying updated RE/flex source code files..."
-cp -r ../reflex/include/reflex/*.h include/reflex
-cp -r ../reflex/unicode/*.cpp lib
-cp -r ../reflex/lib/*.cpp lib
-cp -r ../reflex/fuzzy/fuzzymatcher.h include/reflex
-fi
-
+# if [ -d ../reflex ]; then
+# echo "Copying updated RE/flex source code files..."
+# cp -r ../reflex/include/reflex/*.h include/reflex
+# cp -r ../reflex/unicode/*.cpp lib
+# cp -r ../reflex/lib/*.cpp lib
+# cp -r ../reflex/fuzzy/fuzzymatcher.h include/reflex
+# fi
 # change lib/Makefile.am to use noinst_LIBRARIES
-sed -i .bak 's/lib_LIBRARIES/noinst_LIBRARIES/' lib/Makefile.am
-rm -f lib/Makefile.am.bak
+# sed -i .bak 's/lib_LIBRARIES/noinst_LIBRARIES/' lib/Makefile.am
+# rm -f lib/Makefile.am.bak
 
 # run autoconf and automake stuff with maintainer mode disabled
 aclocal
