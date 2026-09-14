2938102f0909c48cb2e04891cc18d03989c0733c
Robert van Engelen <genivia-inc@users.noreply.github.com>
2026-09-14 08:45:16 +0200

    update -z decompressor to impose xz/lzma limit
    fix #566

--- src/zstream.hpp
+++ src/zstream.hpp
@@ -524,7 +524,8 @@ class zstreambuf : public std::streambuf {
         lzma_strm_->avail_in = zlen_ - zcur_;
 
         // initialize lzma decompress
-        lzma_ret ret = lzma_auto_decoder(lzma_strm_, UINT64_MAX, LZMA_TELL_UNSUPPORTED_CHECK | LZMA_CONCATENATED);
+        uint64_t memlimit = 100 * 1024 * 1024; // 100 MB: files compressed with 'xz -9' require 65 MB to decompress
+        lzma_ret ret = lzma_auto_decoder(lzma_strm_, memlimit, LZMA_TELL_UNSUPPORTED_CHECK | LZMA_CONCATENATED);
         if (ret != LZMA_OK)
         {
           cannot_decompress(pathname_, "lzma_auto_decoder failed");
@@ -1229,7 +1230,8 @@ class zstreambuf : public std::streambuf {
       try
       {
         xzfile_ = new XZ();
-        lzma_ret ret = lzma_auto_decoder(&xzfile_->strm, UINT64_MAX, LZMA_TELL_UNSUPPORTED_CHECK | LZMA_CONCATENATED);
+        uint64_t memlimit = 100 * 1024 * 1024; // 100 MB: files compressed with 'xz -9' require 65 MB to decompress
+        lzma_ret ret = lzma_auto_decoder(&xzfile_->strm, memlimit, LZMA_TELL_UNSUPPORTED_CHECK | LZMA_CONCATENATED);
         if (ret != LZMA_OK)
         {
           warning("lzma_stream_decoder failed", pathname);
@@ -1547,7 +1549,8 @@ class zstreambuf : public std::streambuf {
                 xzfile_ = new XZ();
                 xzfile_->strm.next_in  = buf_;
                 xzfile_->strm.avail_in = 6;
-                lzma_ret ret = lzma_auto_decoder(&xzfile_->strm, UINT64_MAX, LZMA_TELL_UNSUPPORTED_CHECK | LZMA_CONCATENATED);
+                uint64_t memlimit = 100 * 1024 * 1024; // 100 MB: files compressed with 'xz -9' require 65 MB to decompress
+                lzma_ret ret = lzma_auto_decoder(&xzfile_->strm, memlimit, LZMA_TELL_UNSUPPORTED_CHECK | LZMA_CONCATENATED);
                 if (ret != LZMA_OK)
                 {
                   warning("lzma_stream_decoder failed", pathname);
